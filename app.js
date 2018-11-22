437ea6ef51b470db6e6c9556bd81c38ee7ff1007
AmlraEF <44643180+AmlraEF@users.noreply.github.com>
2018-11-22 14:53:03 +0000

    Update app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// Require dependencies
+const express = require('express');
+const path = require('path');
+
+const app = express('express');
+
+// Use EJS as view engine, rendering .ejs files from the views folder
+app.set('view engine', 'ejs');
+// Serve static files (stylesheets, images, etc.) from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/notebook', (req, res) => {
+  res.render('Notebook');
+});
+
+// Catch-all route
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+// Set server to listen on port
+const port = 5000;
+app.listen(port, () => {
+ console.log(`Server is listening on port ${port}`);
+});
